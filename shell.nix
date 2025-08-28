9e8e1dc6785f791b2998ce6a1afea081b4c06f6a
Serge Bazanski <q3k@q3k.org>
2025-08-28 21:51:13 +0200

    *: add rust-toolchain.toml and shell.nix
    This makes builds somewhat reproducible.

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1d53b40cffa7e78db147341de4f9bf2da2bfea9e.tar.gz") {}
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    wchisp wlink rustup
+  ];
+}
